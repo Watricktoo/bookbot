28c6b8058e8d7dd7b8b9484256570697932d7d95
Watricktoo <Watricktwo@gmail.com>
2024-03-26 13:20:57 -0500

    Update main.py from hello world to file opening & reading function

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello world")
+def main():
+    #Opens files and reads contents of book
+    with open("books/frankenstein.txt") as book:
+        book_contents = book.read()
+
+    #Displays contents to the console
+    print(book_contents)
+
+    
+main()
