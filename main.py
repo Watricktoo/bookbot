76f51652dfeb81bc4cf02c8a379078006d6693db
Watricktoo <Watricktwo@gmail.com>
2024-03-27 11:41:39 -0500

    Created Letter count Dict

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 def main():
-    #Opens files and reads contents of book
-    with open("books/frankenstein.txt") as book:
-        book_contents = book.read()
-
+    book_contents = read_book()
     total_word_count = word_count(book_contents)
-    print(total_word_count)
+    letter_dict = count_letters(book_contents)
+    formated_output(total_word_count, letter_dict)
     #Displays contents to the console
     
 
@@ -15,5 +13,25 @@ def word_count(full_text):
         amount_of_words += 1
 
     return amount_of_words
+
+def read_book():
+    #Opens files and reads contents of book
+    with open("books/frankenstein.txt") as book:
+        book_contents = book.read()
+    return book_contents
+
+def count_letters(full_text):
+    letter_count = {}
+    lowercase_text = full_text.lower()
+    for words in lowercase_text:
+        for chars in words:
+            if chars in letter_count:
+                letter_count[chars] += 1
+            else: 
+                letter_count[chars] = 1
+    return letter_count
     
+def formated_output(word_total, letter_dictionary):
+    print(word_total)
+    print(letter_dictionary)
 main()
