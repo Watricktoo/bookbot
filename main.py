8a71648cc342af20a5801bdff548bdf4473c39db
Watricktoo <Watricktwo@gmail.com>
2024-03-27 11:06:44 -0500

    Word count added

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ def main():
     with open("books/frankenstein.txt") as book:
         book_contents = book.read()
 
+    total_word_count = word_count(book_contents)
+    print(total_word_count)
     #Displays contents to the console
-    print(book_contents)
+    
 
+def word_count(full_text):
+    amount_of_words = 0
+    words = full_text.split()
+    for x in range(0, len(words)):
+        amount_of_words += 1
+
+    return amount_of_words
     
 main()
